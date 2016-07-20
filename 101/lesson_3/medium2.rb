e8ee43737a973585112095b94ec1ac7c6d7d0524
Jarold Wong <jaroldw@gmail.com>
2016-07-19 23:35:31 -0700

    complete exercise

--- 101/lesson_3/medium2.rb
+++ 101/lesson_3/medium2.rb
@@ -51,3 +51,68 @@ sentence = "Humpty Dumpty sat on a wall."
 words = sentence.split(/\W/)
 words.reverse!
 reversedwords.join += '.'
+
+# Question 5
+
+answer = 42
+
+def mess_with_it(some_number)
+  some_number += 8
+end
+
+new_answer = mess_with_it(answer)
+
+p answer - 8
+
+#34
+
+# Question 6
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
+
+mess_with_demographics(munsters)
+
+# Question 7
+
+def rps(fist1, fist2)
+  if fist1 == "rock"
+    (fist2 == "paper") ? "paper" : "rock"
+  elsif fist1 == "paper"
+    (fist2 == "scissors") ? "scissors" : "paper"
+  else
+    (fist2 == "rock") ? "rock" : "scissors"
+  end
+end
+
+puts rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock")
+
+# "paper"
+
+# Question 8
+
+def foo(param = "no")
+  "yes"
+end
+
+def bar(param = "no")
+  param == "no" ? "yes" : "no"
+end
+
+bar(foo)
+
+# foo returns "yes", bar("yes") returns "no"
+
+# Question 9
