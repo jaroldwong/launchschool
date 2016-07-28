c7d89d538b222dc4b384921aae9751500459039f
Jarold Wong <jaroldw@gmail.com>
2016-07-27 23:53:09 -0700

    refactor comparison methods

--- 120/lesson_2/oo_rps.rb
+++ 120/lesson_2/oo_rps.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Player
   attr_accessor :move, :name
 
@@ -19,7 +17,7 @@ class Human < Player
     end
     self.name = n
   end
-  
+
   def choose
     choice = nil
     loop do
@@ -36,7 +34,7 @@ class Computer < Player
   def set_name
     self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
   end
-  
+
   def choose
     self.move = Move.new(Move::VALUES.sample)
   end
@@ -44,49 +42,35 @@ end
 
 class Move
   VALUES = ['rock', 'paper', 'scissors']
-  
+
   def initialize(value)
     @value = value
   end
-  
+
   def scissors?
     @value == 'scissors'
   end
-  
+
   def rock?
     @value == 'rock'
   end
-  
+
   def paper?
     @value == 'paper'
   end
-  
+
   def >(other_move)
-    if rock?
-      return true if other_move.scissors?
-      return false
-    elsif paper?
-      return true if other_move.rock?
-      return false
-    elsif scissors?
-      return true if other_move.paper?
-      return false
-    end
+    (rock? && other_move.scissors?) ||
+      (paper? && other_move.rock?) ||
+      (scissors? && other_move.paper?)
   end
-  
+
   def <(other_move)
-    if rock?
-      return true if other_move.paper?
-      return false
-    elsif paper?
-      return true if other_move.scissors?
-      return false
-    elsif scissors?
-      return true if other_move.rock?
-      return false
-    end
+    (rock? && other_move.paper?) ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
   end
-  
+
   def to_s
     @value
   end
@@ -103,16 +87,17 @@ class RPSGame
   def display_welcome_message
     puts "Welcome to Rock, Paper, Scissors!"
   end
-  
+
   def display_goodbye_message
     puts "Thanks for playing Rock, Paper, Scissors!"
   end
-  
-  def display_winner
+
+  def display_moves
     puts "#{human.name} chose #{human.move}."
     puts "#{computer.name} chose #{computer.move}."
+  end
 
-binding.pry
+  def display_winner
     if human.move > computer.move
       puts "#{human.name} won!"
     elsif human.move < computer.move
@@ -121,7 +106,7 @@ binding.pry
       puts "It's a tie!"
     end
   end
-  
+
   def play_again?
     answer = nil
     loop do
@@ -130,15 +115,16 @@ binding.pry
       break if ['y', 'n'].include? answer.downcase
       puts "Sorry, must be y or n."
     end
-    
+
     answer == 'y' ? true : false
   end
-  
+
   def play
     display_welcome_message
     loop do
       human.choose
       computer.choose
+      display_moves
       display_winner
       break unless play_again?
     end
