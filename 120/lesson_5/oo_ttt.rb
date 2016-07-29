9bc38cf4191b93f49b991ec63ee7b3de0e4351bd
Jarold Wong <jaroldw@gmail.com>
2016-07-28 19:44:38 -0700

    add player moves and turns

--- 120/lesson_5/oo_ttt.rb
+++ 120/lesson_5/oo_ttt.rb
@@ -6,54 +6,71 @@
 # marking a square. The first player to mark 3 squares in a row wins.
 
 class Board
-  INITIAL_MARKER = "R"
-  
+  INITIAL_MARKER = " "
+
   def initialize
     @squares = {}
     (1..9).each { |key| @squares[key] = Square.new(INITIAL_MARKER)}
   end
-  
+
   def get_square_at(key)
     @squares[key]
+  end
+
+  def set_square_at(key, marker)
+    @squares[key].marker = marker
+  end
+
+  def unmarked_keys
+    @squares.keys.select { |key| @squares[key].unmarked? }
   end
 end
 
 class Square
+  attr_accessor :marker
+
   def initialize(marker)
     @marker = marker
   end
-  
+
   def to_s
     @marker
+  end
+
+  def unmarked?
+    marker == Board::INITIAL_MARKER
   end
 end
 
 class Player
-  def initialize
-    # maybe a "marker" to keep track of this player's symbol (ie, 'X' or 'O')
-  end
+  attr_reader :marker
 
-  def mark
-
+  def initialize(marker)
+    @marker = marker
   end
 end
 
 class TTTGame
-  attr_reader :board
-  
+  HUMAN_MARKER = "X"
+  COMPUTER_MARKER = "O"
+
+  attr_reader :board, :human, :computer
+
   def initialize
     @board = Board.new
+    @human = Player.new(HUMAN_MARKER)
+    @computer = Player.new(COMPUTER_MARKER)
   end
-  
+
   def display_welcome_message
     puts "Welcome to Tic Tac Toe!"
     puts ""
   end
-  
+
   def display_goodbye_message
     puts "Thanks for playing Tic Tac Toe! Goodbye!"
   end
-  
+
   def display_board
     puts ""
     puts "     |     |"
@@ -69,16 +86,34 @@ class TTTGame
     puts "     |     |"
     puts ""
   end
-  
+
+  def human_moves
+    puts "Choose a square between (#{board.unmarked_keys.join(', ')})"
+    square = nil
+    loop do
+      square = gets.chomp.to_i
+      break if board.unmarked_keys.include?(square)
+      puts "Sorry, that's not a valid choice."
+    end
+
+    board.set_square_at(square, human.marker)
+  end
+
+  def computer_moves
+    board.set_square_at(board.unmarked_keys.sample, computer.marker)
+  end
+
   def play
     display_welcome_message
-    loop do
-      display_board
-      first_player_moves
-      break if someone_won? || board_full?
+    display_board
 
-      second_player_moves
-      break if someone_won? || board_full?
+    loop do
+      human_moves
+      #break if someone_won? || board_full?
+     computer_moves
+      #break if someone_won? || board_full?
+
+      display_board
     end
     display_result
     display_goodbye_message
