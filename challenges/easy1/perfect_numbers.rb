ed27258e98a1cbca17586af205f9a8ddde3417e2
Jarold Wong <jaroldw@gmail.com>
2016-08-11 17:06:22 -0700

    first attempt

--- challenges/easy1/perfect_numbers.rb
+++ challenges/easy1/perfect_numbers.rb
@@ -1,8 +1,29 @@
 class PerfectNumber
-  def initialize(number)
-    @number = number
+  def self.classify(number)
+    raise RuntimeError if number < 0
+    
+    sum_of_factors = factors(number).reduce(&:+)
+
+    case
+    when sum_of_factors == number
+      return "perfect"
+    when sum_of_factors > number
+      return "abundant"
+    when sum_of_factors < number
+      return "deficient"
+    end
   end
 
-  def classify
+  def self.factors(number)
+    factors = []
+    divisor = 1
+
+    loop do
+      factors << divisor if number % divisor == 0
+      divisor += 1
+      break if number == divisor
+    end
+
+    factors
   end
 end
