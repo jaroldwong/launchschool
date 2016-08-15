cdfacf56af5e13ceb60d05da9776a40f4e4d5375
Jarold Wong <jaroldw@gmail.com>
2016-08-15 12:16:39 -0700

    solution

--- challenges/medium1/secret_handshake.rb
+++ challenges/medium1/secret_handshake.rb
@@ -1,7 +1,28 @@
 class SecretHandshake
-  def initialize
+  CODE = ['wink', 'double blink', 'close your eyes', 'jump', 'reverse'].freeze
+
+  def initialize(input)
+    @binary_num = input.to_s(2) if input.class == Fixnum
   end
 
   def commands
+    handshake = []
+    return handshake if @binary_num.nil?
+
+    reversed_binary = @binary_num.reverse
+
+    @binary_num.size.times do |idx|
+      handshake << CODE[idx] if reversed_binary[idx] == "1"
+    end
+
+    if handshake.last == 'reverse'
+      handshake.pop
+      handshake.reverse
+    else
+      handshake
+    end
   end
 end
+
+handshake = SecretHandshake.new(4)
+p handshake.commands
