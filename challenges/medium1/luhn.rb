84b0eff5dee7bb53bfdb3456558caabed9a3c677
Jarold Wong <jaroldw@gmail.com>
2016-08-15 13:24:48 -0700

    first attempt at solution

--- challenges/medium1/luhn.rb
+++ challenges/medium1/luhn.rb
@@ -4,17 +4,52 @@ class Luhn
   end
 
   def addends
-    #returns array
+    digits = @number.to_s.split('').map(&:to_i)
+    check_digit = digits.last
+
+    if digits.size.odd?
+      doubled = digits.each_with_index do |digit, idx|
+        if idx.odd?
+          if (digit * 2) > 9
+            digits[idx] = digit * 2 - 9
+          else
+            digits[idx] = digit * 2
+          end
+        end
+      end
+    else
+      doubled = digits.each_with_index do |digit, idx|
+        if idx.even?
+          if (digit * 2) > 9
+            digits[idx] = digit * 2 - 9
+          else
+            digits[idx] = digit * 2
+          end
+        end
+      end
+    end
+
+    doubled
   end
 
   def checksum
-    #returns number
+    self.addends.reduce(&:+)
   end
 
   def valid?
-    #returns bool
+    return true if self.checksum % 10 == 0
+    false
   end
 
-  def create(number)
+  def self.create(number)
+    digits = number.to_s.split('').map(&:to_i)
+    digits.push(nil)
+
+    (0..9).each do |check_digit|
+      digits[-1] = check_digit
+      break if new(digits.join.to_i).valid?
+    end
+
+    digits.join.to_i
   end
 end
