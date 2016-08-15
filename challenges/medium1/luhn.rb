a2d442feb3886d9de3e6c310584732d1216985d0
Jarold Wong <jaroldw@gmail.com>
2016-08-15 12:35:53 -0700

    skeleton file

---
+++ challenges/medium1/luhn.rb
@@ -0,0 +1,20 @@
+class Luhn
+  def initialize(number)
+    @number = number
+  end
+
+  def addends
+    #returns array
+  end
+
+  def checksum
+    #returns number
+  end
+
+  def valid?
+    #returns bool
+  end
+
+  def create(number)
+  end
+end
