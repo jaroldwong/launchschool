c253ebdcf1b351429fe4613431de800d08a26fb7
Jarold Wong <jaroldw@gmail.com>
2016-08-19 15:34:56 -0700

    add layout file

--- 170/book_viewer/book_viewer.rb
+++ 170/book_viewer/book_viewer.rb
@@ -4,14 +4,14 @@ require "tilt/erubis"
 
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @chapters = File.readlines("data/toc.txt")
+  @contents = File.readlines("data/toc.txt")
   erb :home
 end
 
 get "/chapter/1" do
   @title = "Chapter 1"
-  @chapters = File.readlines("data/toc.txt")
-  @text = File.read("data/chp1.txt")
+  @contents = File.readlines("data/toc.txt")
+  @chapter= File.read("data/chp1.txt")
 
   erb :chapter
 end
