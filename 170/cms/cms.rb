94bfaecd3d5c4c81c14654e9011d541508ac69e9
Jarold Wong <jaroldw@gmail.com>
2016-08-25 15:15:23 -0700

    add index page

--- 170/cms/cms.rb
+++ 170/cms/cms.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+root = File.expand_path("..", __FILE__)
+
 get "/" do
-  "Getting started."
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+  erb :index
 end
